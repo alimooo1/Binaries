53570be3761b0e027f4fa9b27d5e15f6141e8225
Ali <alimoallemi30@gmail.com>
2023-06-21 15:18:21 +0330

    new implementation of textToBinary added

--- src/Binary.ts
+++ src/Binary.ts
@@ -3,13 +3,21 @@ export class Binary {
     this.text = text;
   }
 
-  textToBinary() {
+  oldTextToBinary() {
     const buffer = new ArrayBuffer(this.text.length * 4); // 4 bytes for each char
     const bufferView = new Int32Array(buffer);
     for (let i = 0, strLen = this.text.length; i < strLen; i++) {
       bufferView[i] = this.text.charCodeAt(i);
     }
 
+    return bufferView;
+  }
+
+  newTextToBinary() {
+    const encoder = new TextEncoder();
+    const encodedData = encoder.encode(this.text);
+    const buffer = encodedData.buffer;
+    const bufferView = new Uint8Array(buffer);
     return bufferView;
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,8 @@
-// const numberToBinary = (num: number, base = 2) => {
-//   return Number(num).toString(base);
-// };
-
 import { Binary } from "./Binary";
 
-// const binaryToNumber = (num: string, base = 2) => {
-//   return parseInt(num, base);
-// };
+const binary = new Binary("علی معلمی هستم");
+const base64 = binary.textToBase64();
+const buffer = binary.newTextToBinary();
 
-// const textToBinary = (text: string, base = 2) => {
-//   let output = "";
-//   text.split("").forEach((element) => {
-//     const char = element.charCodeAt(0).toString(base);
-//     output += ("00000000" + char).slice(-8).concat("");
-//   });
-//   return output;
-// };
-
-// console.log(textToBinary("Abbas Boazar"));
-
-const binary = new Binary("Ali");
-console.log(binary.textToBinary());
+console.log(base64);
+console.log(buffer);
